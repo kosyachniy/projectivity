5673ce7dd508320cad205204ae004ed64038b120
Poloz Alexey <polozhev@mail.ru>
2018-03-17 22:54:21 +0300

    Bug with DB

--- app/signup.py
+++ app/signup.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, jsonify
 from app import app
-from db import *
+from mongodb import *
 
 @app.route('/', methods=['POST'])
 def hello():
@@ -8,3 +8,4 @@ def hello():
 	if x['cm'] == 'reg':
 		del x['cm']
 		db.insert(x)
+	return 'Yep'
