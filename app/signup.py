306cb3c08ec5e732120f25230814b03dabcb69e3
Poloz Alexey <polozhev@mail.ru>
2018-03-18 14:08:43 +0300

    Control errors

--- app/signup.py
+++ app/signup.py
@@ -5,7 +5,11 @@ from mongodb import *
 @app.route('/', methods=['POST'])
 def hello():
 	x = request.json
-	if x['cm'] == 'reg':
-		del x['cm']
-		db['users'].insert(x)
-	return 'Yep'
+	print(x)
+	try:
+		if x['cm'] == 'reg':
+			del x['cm']
+			db['users'].insert(x)
+		return 'Yep'
+	except:
+		return 'Error'
