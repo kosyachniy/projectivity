ffc492183914651fc6143752e7540a4ebd9b9bc9
Poloz Alexey <polozhev@mail.ru>
2018-03-20 20:28:53 +0300

    Fixed bugs

--- app/out.py
+++ app/out.py
@@ -3,8 +3,7 @@ from app import app
 
 @app.route('/out')
 def out():
-	print(user)
-	user = {'login': None}
+	#логаут
 
 	print(request.data)
 	return '<script>document.location.href = document.referrer</script>' #redirect(request.url, code=302)
