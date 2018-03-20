d4e09ee2d13098e0e3dc943f0c4346f29c6a4c67
Poloz Alexey <polozhev@mail.ru>
2018-03-20 17:19:25 +0300

    Main page

--- app/index.py
+++ app/index.py
@@ -5,13 +5,13 @@ from app import app
 @app.route('/index')
 def index():
 	competions = [
-		{'name': 'VK Cup', 'author': 'ВКонтакте', 'time': 84931981},
-		{'name': 'Школа анализа данных', 'author': 'Яндекс', 'time': 59257233},
+		{'id': 1, 'name': 'VK Cup', 'author': 'ВКонтакте', 'time': 84931981},
+		{'id': 2, 'name': 'Школа анализа данных', 'author': 'Яндекс', 'time': 59257233},
 	]
 
 	users = [
-		{'name': 'Иван', 'surname': 'Тюрин', 'login': 'ivantt'},
-		{'name': 'Алексей', 'surname': 'Полоз', 'login': 'kosyachniy'},
+		{'name': 'Иван', 'surname': 'Тюрин', 'login': 'ivantt', 'photo': 1},
+		{'name': 'Алексей', 'surname': 'Полоз', 'login': 'kosyachniy', 'photo': 2},
 	]
 
 	user = {
