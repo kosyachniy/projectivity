6c7d90ff081b8906f76a472af7603d44589d8baa
Poloz Alexey <polozhev@mail.ru>
2018-03-20 13:32:00 +0300

    Without OpenSSl

--- run.py
+++ run.py
@@ -1,9 +1,3 @@
 #!flask/bin/python
-
-from OpenSSL import SSL
-context = SSL.Context(SSL.SSLv23_METHOD)
-context.use_privatekey_file('server.key')
-context.use_certificate_file('server.crt')
-
 from app import app
-app.run(host='0.0.0.0', port=80, debug=True, ssl_context=context)
+app.run(host='0.0.0.0', port=80, debug=True, ssl_context=('server.crt', 'server.key'))
