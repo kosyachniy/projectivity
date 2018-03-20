e372f088b8d37a9f97c41030d5084368ce75beb8
Poloz Alexey <polozhev@mail.ru>
2018-03-20 13:26:54 +0300

    TSL

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
 #!flask/bin/python
+
+from OpenSSL import SSL
+context = SSL.Context(SSL.SSLv23_METHOD)
+context.use_privatekey_file('server.key')
+context.use_certificate_file('server.crt')
+
 from app import app
-app.run(host='0.0.0.0', port=80, debug = True)
+app.run(host='0.0.0.0', port=80, debug=True, ssl_context=context)
