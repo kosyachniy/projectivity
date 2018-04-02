dbe8a4bdf656fa1e19f353848d46b000b3361b23
Poloz Alexey <polozhev@mail.ru>
2018-04-02 16:55:13 +0300

    Re

--- app/__init__.py
+++ app/__init__.py
@@ -7,5 +7,5 @@ from app import process
 from app import index
 from app import login
 from app import signin
-from app import out
+#from app import out
 from app import errors
---
+++ app/signup.py
@@ -0,0 +1,20 @@
+from flask import session, request
+from app import app
+
+from requests import post
+
+LINK = 'http://167.99.128.56/'
+
+@app.route('/signin', methods=['POST'])
+def out():
+	x = request.json
+	req = post(LINK, json={'cm': 'profile.auth', 'login': x['login'], 'pass': x['pass']}).text
+
+	if len(req) > 2:
+		session['token'] = req
+		session['login'] = x['login']
+
+		return '<script>document.location.href = document.referrer</script>'
+
+	else:
+		return req
