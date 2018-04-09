a48b2bb53105564d84454240fbfd8b40332c5612
Poloz Alexey <polozhev@mail.ru>
2018-04-09 12:23:51 +0300

    Competion

---
+++ app/competion.py
@@ -0,0 +1,17 @@
+from flask import render_template, session
+from app import app, LINK
+
+from requests import post
+from json import loads
+
+@app.route('/competions/<id>')
+def competions(id):
+	x = loads(post(LINK, json={'cm': 'competions.get', 'id': id}).text)
+
+	return render_template('competion.html',
+		title = x['name'],
+		description = 'Конкурс, стартап, конференция, хакатон, олимпиада, соревнование',
+		url = 'competions/' + id,
+		competion = x,
+		user = {'login': session['login'] if 'token' in session else None},
+	)
