ebcce68fc26fad7b88136894dd65a652de54f3d0
Poloz Alexey <polozhev@mail.ru>
2018-05-27 12:07:24 +0300

    Update without reload

--- app/static/main.js
+++ app/static/main.js
@@ -1,6 +1,10 @@
+// Вызов при изменении размеров экрана
+
 $(function() {
 	$(window).resize(hideHeader);
 });
+
+// Менять отображение хедера
 
 var x = document.body.clientWidth;
 
@@ -16,6 +20,8 @@ function hideHeader() {
 	x = y;
 }
 
+// Хедер при нажатии
+
 function change(min_width=700) {
 	if (document.body.clientWidth <= min_width) {
 		if ($('.u-expand').css('display') == 'block')
@@ -27,44 +33,69 @@ function change(min_width=700) {
 	}
 }
 
+// Сетка статей
+
 function place(elem, count=4, percent=100, margin=0, padding=0, max_width=1500, min_width=590) {
-	var head = document.head || document.getElementsByTagName('head')[0];
-	var style = document.createElement('style');
+var head = document.head || document.getElementsByTagName('head')[0];
+var style = document.createElement('style');
 
-	var ots = margin * 2; /* + padding * 2*/
-	var re = (max_width - min_width) / (count - 1);
-	var obl = max_width * percent / 100;
+var ots = margin * 2; /* + padding * 2*/
+var re = (max_width - min_width) / (count - 1);
+var obl = max_width * percent / 100;
 
-	var css = elem + " {width: " + percent + "%;}\n" + elem + " a > div {width: 100%;}\n" + elem + " > a {margin: " + margin + "px;}\n" + elem + " a > div {padding: " + padding + "px;}\n@media all and (max-width: " + min_width + "px) {" + elem + " {width: 100%;} " + elem + " > a {width: calc(100% - " + ots + "px);}}\n";
+var css = elem + " {width: " + percent + "%;}\n" + elem + " a > div {width: 100%;}\n" + elem + " > a {margin: " + margin + "px;}\n" + elem + " a > div {padding: " + padding + "px;}\n@media all and (max-width: " + min_width + "px) {" + elem + " {width: 100%;} " + elem + " > a {width: calc(100% - " + ots + "px);}}\n";
 
-	for (var i = 0; i < count-1; i++) {
-		css += "@media all and (min-width: " + ~~(min_width + re * i) + "px) {" + elem + " {width: " + percent + "%; margin-left: " + (100 - percent) / 2 + "%;} " + elem + " > a {width: calc(" + 100 / (i + 2) + "% - " + ots + "px);}}\n";
-	}
-
-	css += "@media all and (min-width: " + max_width + "px) {" + elem + " {width: " + obl + "px;} " + elem + " > a {width: " + (obl / count - ots) + "px;}}";
-
-	style.appendChild(document.createTextNode(css));
-
-	head.appendChild(style);
+for (var i = 0; i < count-1; i++) {
+css += "@media all and (min-width: " + ~~(min_width + re * i) + "px) {" + elem + " {width: " + percent + "%; margin-left: " + (100 - percent) / 2 + "%;} " + elem + " > a {width: calc(" + 100 / (i + 2) + "% - " + ots + "px);}}\n";
 }
 
-// function place(elem, count=4, percent=100, margin=0, padding=0, max_width=1500, min_width=425) {
-// 	var head = document.head || document.getElementsByTagName('head')[0];
-// 	var style = document.getElementsByTagName('style')[0] || document.createElement('style');
+css += "@media all and (min-width: " + max_width + "px) {" + elem + " {width: " + obl + "px;} " + elem + " > a {width: " + (obl / count - ots) + "px;}}";
 
-// 	var ots = margin * 2 + padding * 2;
-// 	var re = (max_width - min_width) / (count - 1);
-// 	var obl = max_width * percent / 100;
+style.appendChild(document.createTextNode(css));
 
-// 	var css = elem + " > div {margin: " + margin + "px; padding: " + padding + "px;}\n@media all and (max-width: " + min_width + "px) {" + elem + " {width: 100%;} " + elem + " > div {width: calc(100% - " + ots + "px);}}\n";
+head.appendChild(style);
+}
 
-// 	for (var i = 0; i < count-1; i++) {
-// 		css += "@media all and (min-width: " + ~~(min_width + re * i) + "px) {" + elem + " {width: " + percent + "%; margin-left: " + (100 - percent) / 2 + "%;} " + elem + " > div {width: calc(" + 100 / (i + 2) + "% - " + ots + "px);}}\n";
-// 	}
+// Отображение контента без перезагрузки
 
-// 	css += "@media all and (min-width: " + max_width + "px) {" + elem + " {width: " + obl + "px; margin-left: calc(50vw - " + obl / 2 + "px);} " + elem + " > div {width: " + (obl / count - ots) + "px;}}";
+function showContent(link) {  
+	var cont = document.getElementById('contentBody');
+	var loading = document.getElementById('loading');
 
-// 	style.appendChild(document.createTextNode(css));
+	cont.innerHTML = loading.innerHTML;
 
-// 	head.appendChild(style);
-// }
+	var http = createRequestObject();
+	if (http) {
+		http.open('get', link);
+		http.onreadystatechange = function () {  
+			if(http.readyState == 4) {
+				cont.innerHTML = http.responseText;
+			}
+		}
+
+	http.send(null);
+	} else {
+		document.location = link;
+	}
+}
+ 
+function createRequestObject() {
+	try {
+		return new XMLHttpRequest()
+	} catch(e) {
+		try {
+			return new ActiveXObject('Msxml2.XMLHTTP')
+		} catch(e) {
+			try {
+				return new ActiveXObject('Microsoft.XMLHTTP')
+			} catch(e) {
+				return null;
+			}
+		}
+	}
+}
+
+function href(url) {
+	history.pushState(null, null, url);
+	showContent(url);
+}
