128f227e0fe9d74ca40dda344c01d5db2e202d36
Poloz Alexey <polozhev@mail.ru>
2018-06-06 00:33:32 +0300

    Test transition

--- app/static/main.js
+++ app/static/main.js
@@ -36,24 +36,24 @@ function change(min_width=700) {
 // Сетка статей
 
 function place(elem, count=4, percent=100, margin=0, padding=0, max_width=1500, min_width=590) {
-var head = document.head || document.getElementsByTagName('head')[0];
-var style = document.createElement('style');
+	var head = document.head || document.getElementsByTagName('head')[0];
+	var style = document.createElement('style');
 
-var ots = margin * 2; /* + padding * 2*/
-var re = (max_width - min_width) / (count - 1);
-var obl = max_width * percent / 100;
+	var ots = margin * 2; /* + padding * 2*/
+	var re = (max_width - min_width) / (count - 1);
+	var obl = max_width * percent / 100;
 
-var css = elem + " {width: " + percent + "%;}\n" + elem + " a > div {width: 100%;}\n" + elem + " > a {margin: " + margin + "px;}\n" + elem + " a > div {padding: " + padding + "px;}\n@media all and (max-width: " + min_width + "px) {" + elem + " {width: 100%;} " + elem + " > a {width: calc(100% - " + ots + "px);}}\n";
+	var css = elem + " {width: " + percent + "%;}\n" + elem + " a > div {width: 100%;}\n" + elem + " > a {margin: " + margin + "px;}\n" + elem + " a > div {padding: " + padding + "px;}\n@media all and (max-width: " + min_width + "px) {" + elem + " {width: 100%;} " + elem + " > a {width: calc(100% - " + ots + "px);}}\n";
 
-for (var i = 0; i < count-1; i++) {
-css += "@media all and (min-width: " + ~~(min_width + re * i) + "px) {" + elem + " {width: " + percent + "%; margin-left: " + (100 - percent) / 2 + "%;} " + elem + " > a {width: calc(" + 100 / (i + 2) + "% - " + ots + "px);}}\n";
-}
+	for (var i = 0; i < count-1; i++) {
+		css += "@media all and (min-width: " + ~~(min_width + re * i) + "px) {" + elem + " {width: " + percent + "%; margin-left: " + (100 - percent) / 2 + "%;} " + elem + " > a {width: calc(" + 100 / (i + 2) + "% - " + ots + "px);}}\n";
+	}
 
-css += "@media all and (min-width: " + max_width + "px) {" + elem + " {width: " + obl + "px;} " + elem + " > a {width: " + (obl / count - ots) + "px;}}";
+	css += "@media all and (min-width: " + max_width + "px) {" + elem + " {width: " + obl + "px;} " + elem + " > a {width: " + (obl / count - ots) + "px;}}";
 
-style.appendChild(document.createTextNode(css));
+	style.appendChild(document.createTextNode(css));
 
-head.appendChild(style);
+	head.appendChild(style);
 }
 
 // Отображение контента без перезагрузки
@@ -95,7 +95,7 @@ function createRequestObject() {
 	}
 }
 
-function href(url) {
+function ahref(url) {
 	history.pushState(null, null, url);
 	showContent(url);
 }
